4cfcb0a9851bb34bc40093f810d4a0dba50ada9e
Jose Carlos <jcolscript@gmail.com>
2018-12-03 18:39:14 -0200

    flaha no engano, app.use(cors())

--- src/app.js
+++ src/app.js
@@ -24,12 +24,12 @@ mongoose.connect(config.connerctionString, {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false})) // espaço = %20. Aqui podemos utilizar queryString.
-app.use(bodyParser.json({
+app.use(bodyParser.json({ // Limite de upload de requisições
   limit: '5mb'
 }));
 
 // Habilita o CORS
-app.user(cors());
+app.use(cors());
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
