f6a346968a7b7274fd2b2fc690b2293d81a8d454
Jose Carlos <jcolscript@gmail.com>
2018-12-10 03:53:50 -0200

    limit access backend

--- src/app.js
+++ src/app.js
@@ -15,9 +15,8 @@ app.use(bodyParser.json({ // Limite de upload de requisições
 
 
 // Habilita o CORS
-app.use(cors());
+app.use(cors({origin: 'https://gringottsbanks.ddns.net'}));
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
